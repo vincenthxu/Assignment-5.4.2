37e3832b7fb9aa7e5804a661b3512ee86125fc33
Vincent Xu <vincent.xu1@outlook.com>
2025-08-07 13:15:09 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Assignment_5._4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] matrix = [[1,2],[3,4]];
+
+            Console.WriteLine("The matrix is: ");
+            foreach (var row in matrix)
+            {
+                foreach (var element in row)
+                {
+                    Console.Write($"{element} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Addition of the right diagonal elements is: {SumMatrixRightDiagonal(matrix)}");
+        }
+
+        static int SumMatrixRightDiagonal(int[][] matrix)
+        {
+            int sum = 0;
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                sum += matrix[r][r];
+            }
+            return sum;
+        }
+    }
+}
