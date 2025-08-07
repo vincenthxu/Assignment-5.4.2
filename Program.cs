7aad4d5fcf0dcc79afee07ecf2340cc023f60d57
Vincent Xu <vincent.xu1@outlook.com>
2025-08-07 13:36:03 -0700

    Replaced hardcoded test case with randomly generated test case

--- Program.cs
+++ Program.cs
@@ -4,14 +4,26 @@
     {
         static void Main(string[] args)
         {
-            int[][] matrix = [[1,2],[3,4]];
+            Random rng = new Random();
+
+            int size = rng.Next(2, 5);
+            int[][] matrix = new int[size][];
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = new int[size];
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j] = rng.Next(10);
+                }
+            }
 
             Console.WriteLine("The matrix is: ");
             foreach (var row in matrix)
             {
-                foreach (var element in row)
+                foreach (var item in row)
                 {
-                    Console.Write($"{element} ");
+                    Console.Write($"{item} ");
                 }
                 Console.WriteLine();
             }
@@ -21,9 +33,9 @@
         static int SumMatrixRightDiagonal(int[][] matrix)
         {
             int sum = 0;
-            for (int r = 0; r < matrix.GetLength(0); r++)
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                sum += matrix[r][r];
+                sum += matrix[i][i];
             }
             return sum;
         }
